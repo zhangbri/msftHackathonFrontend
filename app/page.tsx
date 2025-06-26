51dcf973ebb5d4176c8d8609fe377142c588f705
Brian Zhang <zhangbri@umich.edu>
2025-06-26 18:47:29 -0400

    .mov rotated but the video options are upsidedown

--- app/page.tsx
+++ app/page.tsx
@@ -159,6 +159,7 @@ export default function LandingPage() {
                           ref={videoRef}
                           src={videoURL}
                           className={`absolute inset-0 w-full h-full object-contain z-10 bg-black ${rotateVideo ? "rotate-180" : ""}`}
+                          
                           playsInline
                           muted={muted}
                           autoPlay
@@ -247,10 +248,9 @@ export default function LandingPage() {
             </div>
         </section>
       </main>
-
       {/* Footer */}
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-gray-500">© 2024 FormSight. All rights reserved.</p>
+        <p className="text-xs text-gray-500">© 2025 FormSight. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
             Terms of Service
@@ -263,7 +263,6 @@ export default function LandingPage() {
           </Link>
         </nav>
       </footer>
-
     </div>
   )
 }
